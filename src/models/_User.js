fde4b34f39481a76b5671f3b6da0967a555b7d52
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2023-04-23 14:05:57 +0700

    [model][User] init User DB

---
+++ src/models/_User.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const lodash = require('lodash');
+//
+const { HASH_TIMES } = require('../utils');
+const { slug } = require('../utils');
+//
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email wrong format!")
+      }
+    }
+  },
+  userType: {
+    type:String,
+    enum: ['ADMIN', 'LANDLORD', 'TENANT'],
+    required: true,
+  },
+  houseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'House',
+  },
+  roomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+  },
+  isDeleted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  token: {
+    type: String,
+  }
+}, {
+  timestamps: true,
+});
+// # Methods
+userSchema.statics.findByCredentials = async (userName, password, more) => {
+  const user = await User.findOne({ 
+    userName,
+  });
+  if (!user) {
+    throw new Error('This user name doesn\'t exist!');
+  }
+  //
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Wrong password!');
+  }
+  //
+  return user;
+}
+//
+userSchema.methods.toJSON = function () {
+  const PICK_FIELDS = ["_id", "userName", "fullName", "email", "phone", "userType", "houseId", "roomId"];
+  //
+  const user = this;
+  const userObj = lodash.pick(user, PICK_FIELDS);
+  //
+  return userObj;
+}
+// # Middle-wares
+//
+userSchema.pre('save', async function (next) {
+  const user = this;
+  //
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, HASH_TIMES);
+  }
+  //
+  user.slug = slug(user.fullName);
+  //
+  next();
+});
+//
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+  User
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,20 @@
 const PORT = 4000;
 const PATH = '/api';
+const HASH_TIMES = 8;
+
+const slug = function (str, more) {
+  return slugify(str, {
+    replacement: '-',
+    lower: true,
+    locale: 'vi',
+    trim: true
+  })
+}
 
 module.exports = {
   PORT,
   PATH,
+  HASH_TIMES,
+  //
+  slug,
 }
